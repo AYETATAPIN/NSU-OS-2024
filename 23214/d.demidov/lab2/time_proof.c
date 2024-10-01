c66617f040b71bc951ce134bf58a40f291654254
“Dmitriy <d.demidov@g.nsu.ru>
2024-10-01 12:56:08 +0700

    Create time_proof.c

---
+++ 23214/d.demidov/lab2/time_proof.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+extern char* tzname[];
+
+int main() {
+    time_t now;
+    struct tm* sp;
+    (void)time(&now);
+    setenv("TZ","UTC",1);
+    printf("%lld\n", now);
+    printf("%s", ctime(&now));
+    printf("%lld\n", now);
+    sp = localtime(&now);
+    printf("%d/%d/%02d %d:%02d %s\n",
+        sp->tm_mon + 1, sp->tm_mday,
+        sp->tm_year + 1900, sp->tm_hour,
+        sp->tm_min, tzname[sp->tm_isdst]);
+    //printf("%lld\n", (sp->tm_year + 1900) * 365 * 24 * 60 * 60 + sp->tm_mon * 31 * 24 * 60 *  + sp->tm_min * 60)
+    return 0;
+}
